f7689686cefc2348880f2e41bc886a670a221c5e
emmikoivisto <emetsi@gmail.com>
2020-11-28 15:05:42 +0000

    shop tests added

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -8,10 +8,12 @@ public class ShopTest {
     Shop shop;
     Clarinet clarinet;
     Trombone trombone;
+    Violin violin;
 
     @Before
     public void before(){
         shop = new Shop("Music Shop");
+        violin = new Violin("String", "Wood", "wooden", 670, 800, "Alto");
         trombone = new Trombone("Brass", "brass", "brassy", 900, 1500, "rotary");
         clarinet = new Clarinet("Woodwind", "Wood", "wooden", 450, 500, "B");
     }
@@ -22,14 +24,16 @@ public class ShopTest {
     }
 
     @Test
-    public void canAddInstrument(){
-
+    public void canSetName(){
+        shop.setName("Notes Funny");
+        assertEquals("Notes Funny", shop.getName());
     }
 
-//    @Test
-//    public void canAddInstrument(){
-//        shop.addInstrument(clarinet);
-//        shop.addInstrument(trombone);
-//        assertEquals(2, shop.getStockCount());
-//    }
+    @Test
+    public void canAddInstrument(){
+        shop.addInstrument(clarinet);
+        shop.addInstrument(trombone);
+        shop.addInstrument(violin);
+        assertEquals(3, shop.getStockCount());
+    }
 }
