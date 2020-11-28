1d769c2ca43c1593b082ec7deb86f9eb81b8c6d6
emmikoivisto <emetsi@gmail.com>
2020-11-28 12:29:51 +0000

    getters and setters for Instrument

--- src/main/java/Instrument.java
+++ src/main/java/Instrument.java
@@ -8,4 +8,28 @@ public abstract class Instrument {
         this.material = material;
         this.colour = colour;
     }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
--- src/main/java/Trombone.java
+++ src/main/java/Trombone.java
@@ -1,8 +1,20 @@
-public class Trombone extends Instrument {
+public class Trombone extends Instrument implements IPlay  {
     private String valveType;
 
     public Trombone(String family, String material, String colour, String valveType){
         super(family, material, colour);
+        this.valveType = valveType;
+    }
+
+    public String play(String sound){
+        return "Tööt";
+    }
+
+    public String getValveType() {
+        return valveType;
+    }
+
+    public void setValveType(String valveType) {
         this.valveType = valveType;
     }
 }
